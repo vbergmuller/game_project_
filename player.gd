616f50de9a6eb9513b7417cff44343a7f9146fef
Victor Bergmuller de Figueredo <vfigueredo6677@gmail.com>
2025-12-14 23:51:11 -0300

    Movimentação(global) do personagem, animação e flip de animação

--- player.gd
+++ player.gd
@@ -1,16 +1,4 @@
-extends CharacterBody2D
+extends BaseCharacter
 
-const max_speed: int = 120
-const acceleration: int = 50
-const friction: int = 10
-
-func _physics_process(delta: float) -> void:
-	var input = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	).normalized()
-	
-	var lerp_weight = delta * (acceleration if input else friction)
-	velocity = lerp(velocity, input * max_speed, lerp_weight)
-	
-	move_and_slide()
+func _physics_process(_delta: float) -> void:
+	get_input(400, $AnimatedSprite2D)
